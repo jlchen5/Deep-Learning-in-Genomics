fdb0764d84985d36e6b36ec5d9717cc9288b42b8
jlchen5 <jl_chen1@outlook.com>
2022-11-04 11:17:49 +0800

    Create test.py

---
+++ CNN/test.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Dropout, Flatten, Dense
+from tensorflow.keras import Model
